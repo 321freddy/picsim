940456bfada658135a0253362f832410e74dd927
321freddy <frederik.ruess@web.de>
2019-04-02 10:23:29 +0200

    File dropdown

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -1,19 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIController : MonoBehaviour
 {
+    public const string PATH = ".\\TestProgramme\\";
+
+    public Dropdown fileDropdown;
+
+
+    // Start is called before the first frame update
     // Start is called before the first frame update
     void Start()
     {
-        
+        var info = new DirectoryInfo(PATH);
+        var fileInfo = info.GetFiles();
+        foreach (var file in fileInfo)
+        {
+            fileDropdown.options.Add(new Dropdown.OptionData(file.Name));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // Debug.Log("test log 123");
+    }
+
+    public void onFileSelected()
+    {
+        var name = fileDropdown.options[fileDropdown.value].text;
+        Debug.Log("selected file " + name);
     }
 
     public void onBtnClick()
