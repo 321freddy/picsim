2ae16fb4b1c724391094c196237fadb7fef2069a
321freddy <frederik.ruess@web.de>
2019-04-03 14:59:02 +0200

    text anzeige

---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+public static class FileManager
+{
+    public const string PATH = ".\\TestProgramme\\";
+    
+
+    // List all files in Program Directory
+    public static FileInfo[] listAll()
+    {
+        var info = new DirectoryInfo(PATH);
+        return info.GetFiles();
+    }
+
+    // Returns list of all lines in a file
+    public static List<string> getLines(string filename)
+    {
+        var lines = new List<string>();
+        var reader = new StreamReader(PATH + filename, Encoding.GetEncoding(1252));
+        string line;
+
+        while((line = reader.ReadLine()) != null)
+        {
+            lines.Add(line);
+        }
+
+        return lines;
+    }
+}
