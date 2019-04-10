1a0f7f03eae3d88ff09b58766f2e42ec9ad10f57
321freddy <frederik.ruess@web.de>
2019-04-10 21:01:56 +0200

    Program step controls

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -17,7 +17,9 @@ public class GUIController : MonoBehaviour
     public GameObject codeLineTemplate;
 
     private Simulation simulation;
-    private Command lastCommand = null;
+    private Command currentCommand = null;
+    private bool simulationRunning = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -40,55 +42,114 @@ public class GUIController : MonoBehaviour
             }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (simulation != null)
-        {
-            setCommandColor(lastCommand, COLOR_WAS_RUNNING); // Update current command color to green
-            lastCommand = simulation.getCurrentCommand();
-            setCommandColor(lastCommand, COLOR_RUNNING);
-
-            var success = simulation.step();
-            if (success)
-            {
-                // Update GUI
-                GameObject.Find("wRegister").GetComponent<Text>().text ="W-Register: " + simulation.Memory.w_Register.ToString("X");
-                GameObject.Find("ZeroFlag").GetComponent<Text>().text ="Zero-Flag: " + simulation.Memory.ZeroFlag.ToString();
-                GameObject.Find("ProgCounter").GetComponent<Text>().text ="PCL: " + simulation.Memory.ProgramCounter.ToString();
-            }
-            else
-            {
-                simulation = null;
-                lastCommand = null;
-            }
-
-        }
-    }
-
+    
     public void onFileSelected()
     {
         // clear file view
         foreach (Transform child in codeContainer.transform) Destroy(child.gameObject);
 
-        var filename = fileDropdown.options[fileDropdown.value].text;
+        var filename = fileDropdown.options[fileDropdown.value].text; // Get file name
         Debug.Log("selected file " + filename);
-        
-        var lines = FileManager.getLines(filename);
-        
 
-        foreach (string line in lines)
+        var lines = FileManager.getLines(filename); // Read file
+        simulation = Simulation.CreateFromProgram(lines); // Init simulation
+
+        simulationRunning = false;
+        currentCommand = simulation.getCurrentCommand();
+
+        var lineNum = 0;
+        foreach (string line in lines) // Populate code box
         {
-            var lineObject = Instantiate(codeLineTemplate, codeContainer.transform);
-            lineObject.GetComponent<Text>().text = line;
+            var lineObject = Instantiate(codeLineTemplate, codeContainer.transform).GetComponent<Text>();
+            lineObject.text = line;
+
+            if (lineNum == currentCommand.Line) lineObject.color = COLOR_RUNNING; // Set color to green if first command
+            lineNum++;
         }
 
-        simulation = Simulation.CreateFromProgram(lines);
     }
 
-    public void onBtnClick()
+    public void stepIn()
     {
-        Debug.Log("clicked button!!");
+        var success = simulation.step(); // Run command
+        if (success)
+        {
+            // Update GUI
+            setCommandColor(currentCommand, COLOR_WAS_RUNNING); // Update next command color to green
+            currentCommand = simulation.getCurrentCommand();
+            setCommandColor(currentCommand, COLOR_RUNNING);
+
+            GameObject.Find("wRegister").GetComponent<Text>().text = "W Register: " + simulation.Memory.w_Register.ToString("X");
+            GameObject.Find("ZeroFlag").GetComponent<Text>().text = "Zero Flag: " + simulation.Memory.ZeroFlag.ToString();
+            GameObject.Find("ProgCounter").GetComponent<Text>().text = "Program Counter: " + simulation.Memory.ProgramCounter.ToString();
+        }
+        else
+        {
+            // Program reached end
+            simulationRunning = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (simulationRunning)
+        {
+            stepIn();
+        }
+    }
+
+    public void onGoBtnClick()
+    {
+        Debug.Log("Running program");
+        simulationRunning = !simulationRunning; // Toggle
+
+        if (simulationRunning)
+        {
+            GameObject.Find("GoButton").GetComponentInChildren<Text>().text = "Pause";
+        }
+        else
+        {
+            GameObject.Find("GoButton").GetComponentInChildren<Text>().text = "Go";
+        }
+    }
+
+    public void onStepInBtnClick()
+    {
+        if (!simulationRunning)
+        {
+            Debug.Log("Step in");
+            stepIn();
+        }
+    }
+
+    public void onStepOutBtnClick()
+    {
+        if (!simulationRunning)
+        {
+            Debug.Log("Step out");
+            // ...
+        }
+    }
+
+    public void onResetBtnClick()
+    {
+        Debug.Log("Reset program");
+        simulation.Reset();
+
+        // Reset code color
+        foreach (var codeLine in codeContainer.transform.GetComponentsInChildren<Text>())
+        {
+            codeLine.color = COLOR_DEFAULT;
+        }
+
+        // Reset text
+        GameObject.Find("wRegister").GetComponent<Text>().text = "W Register: 0";
+        GameObject.Find("ZeroFlag").GetComponent<Text>().text = "Zero Flag: 0";
+        GameObject.Find("ProgCounter").GetComponent<Text>().text = "Program Counter: 0";
+
+        simulationRunning = false;
+        currentCommand = simulation.getCurrentCommand();
+        setCommandColor(currentCommand, COLOR_RUNNING); // Mark first command
     }
 }
